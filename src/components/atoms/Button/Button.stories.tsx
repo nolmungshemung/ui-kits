74e0d5dc400a5d9d0c96ecfca2de9b2d76728aca
jtpark <vigabhan1231@gmail.com>
2021-12-28 23:39:25 +0900

    feat(story): Button component stories add Button 컴포넌트 스토리 추가
    close #7

--- src/components/atoms/Button/Button.stories.tsx
+++ src/components/atoms/Button/Button.stories.tsx
@@ -5,11 +5,38 @@ import Button from './Button';
 export default {
   title: 'Atoms/Button',
   component: Button,
+  argTypes: {
+    size: {
+      options: ['xs', 'sm', 'md', 'lg', 'xl'],
+      control: { type: 'select' },
+    },
+    color: {
+      options: ['white', 'primary'],
+      control: { type: 'select' },
+    },
+    outline: {
+      options: ['black', 'gray', 'none'],
+      control: { type: 'select' },
+    },
+  },
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<any> = (args) => (
   <Button {...args}>{'Button'}</Button>
 );
 
-export const Default = Template.bind({});
-Default.args = {};
+export const WhiteButton = Template.bind({});
+
+export const PrimaryButton = Template.bind({});
+
+WhiteButton.args = {
+  size: 'md',
+  color: 'white',
+  outline: 'black',
+};
+
+PrimaryButton.args = {
+  size: 'md',
+  color: 'primary',
+  outline: 'none',
+};
